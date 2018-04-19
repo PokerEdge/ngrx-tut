4b225a3284a8b45474a421a43556777a234a9463
Reid <Reid@pokersprout.com>
2018-04-19 19:28:33 -0400

    Created reducer
    Created reducer to manage state changes within the applications by
    analyzing state changes compared against the therein defined initial
    state.

---
+++ src/reducers/tutorial.reducers.ts
@@ -0,0 +1,17 @@
+import { Action } from '@ngrx/store'
+import { Tutorial } from './../models/tutorial.model'
+import * as TutorialActions from './../actions/tutorial.actions'
+
+const intialState: Tutorial = {
+  name: 'Initial Tutorial',
+  url: 'https://google.com'
+}
+
+export function tutReducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
+  switch(action.type) {
+    case TutorialActions.ADD_TUTORIAL:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
